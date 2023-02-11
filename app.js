31d6b7ff154ccd3c2a3905e8c1d3f5fe8026bad1
Faizal Fernandi <faizal.fernandi@togu.co.id>
2023-02-11 08:51:18 +0700

    add all dependencies and config

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var rulesRouter = require('./routes/rules');
 
 var app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
